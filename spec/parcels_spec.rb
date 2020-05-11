eea721431c32a63e639a30261c81a1905a0e5417
Allison Mackey <a.mackey693@gmail.com>
2020-05-11 14:11:13 -0700

    write & test spec #1, expect test to fail

--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,2 +1,10 @@
 require 'rspec'
 require 'parcels_logic'
+
+describe "#Parcels" do
+  describe('#weight')
+   it("allows user to enter a weight") do
+    parcel = Parcel.new("12")
+    expect(parcel.weight?()).to(eq(12))
+   end
+end
